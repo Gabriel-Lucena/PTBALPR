24c3f1c37903c97a38c7a78e0a27a4f85c83af93
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-05-10 12:02:58 -0300

    Correções

--- 9mai/exe/exercicio4.c
+++ 9mai/exe/exercicio4.c
@@ -3,7 +3,10 @@
 int main()
 {
 
-    int numero = 11, i = 1, contador = 0;
+    int numero, i = 1, contador = 0;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
 
     while (i <= numero)
     {
@@ -18,6 +21,10 @@ int main()
     if (contador == 2)
     {
         printf("%d e' primo", numero);
+    }
+    else
+    {
+        printf("%d nao e' primo", numero);
     }
 
     return 0;
