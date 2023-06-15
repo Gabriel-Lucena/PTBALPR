b84a133d27213be997a369cd4bbd68d38a1d2a94
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-06-15 16:39:56 -0300

    Duplicata

--- 13jun/exe/duplicata.c
+++ 13jun/exe/duplicata.c
@@ -6,32 +6,24 @@ void duplicata(int vetor[], int tamanho)
 {
   for (int i = 0; i < tamanho; i++)
   {
-    if (i == 0)
+    int boolean = 1;
+
+    for (int j = i; j <= 1; j--)
     {
-      printf("%g", vetor[i]);
+      if (j == i)
+      {
+        break;
+      }
+      else if ((vetor[i] == vetor[j]) && (i != j))
+      {
+        boolean = 0;
+        break;
+      }
     }
-    else
+
+    if (boolean == 1 || i == 0)
     {
-
-      int boolean = 0;
-
-      for (int j = i - 1; j <= 0; j--)
-      {
-
-        if (vetor[i] == vetor[j])
-        {
-          break;
-        }
-        else
-        {
-          boolean = 1;
-        }
-      }
-
-      if (boolean)
-      {
-        printf("%g", vetor[i]);
-      }
+      printf("%d ", vetor[i]);
     }
   }
 }
@@ -42,9 +34,11 @@ int main()
 
   for (int i = 0; i < 6; i++)
   {
-    printf("Digite o elemento %d:\n");
-    scanf("%lf", numeros[i]);
+    printf("Digite o elemento %d:\n", i);
+    scanf("%d", &numeros[i]);
   }
+
+  duplicata(numeros, 6);
 
   return 0;
 }
